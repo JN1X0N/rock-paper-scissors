ea0b5fdd6c8b0e4cf8e66929ab28764ae9935ed6
JN <nixonspreull@googlemail.com>
2022-07-06 16:19:35 +0100

    added basic ui

--- script.js
+++ script.js
@@ -4,15 +4,7 @@ function computerPlay() {
     return options[Math.floor(Math.random() * options.length)];
 }
 
-function userPlay() {
-    let input = "";
-    while (input = prompt("select one of the following: rock, paper or scissors: ").toLowerCase(), !options.includes(input)) {
-        console.log("invalid choice");
-    }
-    return input;
-}
-
-function win(user, comp) {
+function check(user, comp) {
     if ((user == options[0] && comp == options[2]) ||
         (user == options[1] && comp == options[0]) ||
         (user == options[2] && comp == options[1])) {
@@ -22,40 +14,71 @@ function win(user, comp) {
     }
 }
 
-function playRound() {
+function clearElements(winner) {
+    document.querySelector(".buttons").remove();
+    document.querySelector(".round-results").remove();
+    let result = document.createElement("p");
+    result.textContent = `the ${winner} wins!`;
+    result.style.fontSize = "60px";
+    const body = document.querySelector("body");
+    body.appendChild(result);
+}
+
+function playRound(choice) {
     let computerChoice = computerPlay();
-    let userChoice = userPlay();
+    let userChoice = choice;
+
+    const gameResults = document.querySelector(".game-results p");
+    let scores = gameResults.textContent.split(" - ");
+    const roundResults = document.querySelector(".round-results p")
 
     if (userChoice == computerChoice) {
-        console.log("Draw!");
-        return 0
-    } else if (win(userChoice, computerChoice)) {
-        console.log(`You win! ${userChoice} beats ${computerChoice}`);
-        return 1;
+        roundResults.textContent = "It was a draw!";
+        return;
+    } else if (check(userChoice, computerChoice)) {
+        roundResults.textContent = `You win! ${userChoice} beats ${computerChoice}`;
+        scores[0] = parseInt(scores[0])+1;
     } else {
-        console.log(`You lose! ${computerChoice} beats ${userChoice}`);
-        return -1;
+        roundResults.textContent = `You lose! ${computerChoice} beats ${userChoice}`;
+        scores[1] = parseInt(scores[1])+1;
     }
+
+    if (scores[0] == 5) {
+        clearElements("player");
+    } else if (scores[1] == 5) {
+        clearElements("computer");
+    }
+    
+    gameResults.textContent = scores.join(" - ");
 }
 
-function game() {
-    let userWins = 0;
-    let compWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let result = playRound();
-        if (result == 1) {
-            userWins++;
-        } else if (result == -1) {
-            compWins++;
-        }
-    }
-    if (userWins > compWins) {
-        console.log(`You win with ${userWins} win${userWins != 1 ? "s" : ''} to the computers ${compWins} win${compWins != 1 ? "s" : ''}!`);
-    } else if (userWins < compWins) {
-        console.log(`The computer wins with ${compWins} win${compWins != 1 ? "s" : ''} to your ${userWins} win${userWins != 1 ? "s" : ''}!`);
-    } else {
-        console.log(`You and the computer draw with ${userWins} win${userWins != 1 ? "s" : ''} each!`)
-    }
-}
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(each => each.addEventListener("click", () => {
+    playRound(each.value);
+}));
 
-game();
+
+
+
+
+// function game() {
+//     let userWins = 0;
+//     let compWins = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound();
+//         if (result == 1) {
+//             userWins++;
+//         } else if (result == -1) {
+//             compWins++;
+//         }
+//     }
+//     if (userWins > compWins) {
+//         console.log(`You win with ${userWins} win${userWins != 1 ? "s" : ''} to the computers ${compWins} win${compWins != 1 ? "s" : ''}!`);
+//     } else if (userWins < compWins) {
+//         console.log(`The computer wins with ${compWins} win${compWins != 1 ? "s" : ''} to your ${userWins} win${userWins != 1 ? "s" : ''}!`);
+//     } else {
+//         console.log(`You and the computer draw with ${userWins} win${userWins != 1 ? "s" : ''} each!`)
+//     }
+// }
+
+// game();
